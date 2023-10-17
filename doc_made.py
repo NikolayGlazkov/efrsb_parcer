bfa2b2c421441759757cf18a36c95a20b35883ba
NikolayGlazkov <gn9377419582@gmail.com>
2023-10-18 00:02:45 +0300

    i whos made new func file

--- doc_made.py
+++ doc_made.py
@@ -3,16 +3,22 @@ from docx.shared import Pt
 from docxtpl import DocxTemplate
 
 
-def made_docx_file(data_from_pars:dict,proces:str,lot_namber): # словарь мз парсера заходит сюда и взовисимости от типа торгов пишет файл
-        doc = DocxTemplate("Agent_dogovor_template.docx")
+def made_docx_file(data_from_pars:dict,proces:str,lot_namber,filename:str): # словарь мз парсера заходит сюда и взовисимости от типа торгов пишет файл
+        doc = DocxTemplate(filename)
         doc.render(data_from_pars)
-        doc.save(f'Агентский договор лот№{lot_namber}.docx')
+        doc.save(f'{filename} лот№{lot_namber}.docx')
 
-        if proces == "Открытый аукцион":
-            doc = DocxTemplate("Zayavka_auction.docx")
-            doc.render(data_from_pars)
-            doc.save(f'заявка аукцион лот№{lot_namber}.docx')
-        if proces == "Публичное предложение": 
-            doc = DocxTemplate("Zayavka_pablic.docx")
-            doc.render(data_from_pars)
-            doc.save(f'заявка публичка лот№{lot_namber}.docx')
+
+
+doc = DocxTemplate("Agent_dogovor_template.docx")
+doc.render(data_from_pars)
+doc.save(f'Агентский договор лот№{lot_namber}.docx')
+
+if proces == "Открытый аукцион":
+    doc = DocxTemplate("Zayavka_auction.docx")
+    doc.render(data_from_pars)
+    doc.save(f'заявка аукцион лот№{lot_namber}.docx')
+if proces == "Публичное предложение": 
+    doc = DocxTemplate("Zayavka_pablic.docx")
+    doc.render(data_from_pars)
+    doc.save(f'заявка публичка лот№{lot_namber}.docx')
