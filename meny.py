033ec384d0d391b90ed6355e498d436b5c60d494
NikolayGlazkov <gn9377419582@gmail.com>
2023-10-18 00:11:14 +0300

    meny file made

---
+++ meny.py
@@ -0,0 +1,13 @@
+# doc = DocxTemplate("Agent_dogovor_template.docx")
+# doc.render(data_from_pars)
+# doc.save(f'Агентский договор лот№{lot_namber}.docx')
+
+# if proces == "Открытый аукцион":
+#     doc = DocxTemplate("Zayavka_auction.docx")
+#     doc.render(data_from_pars)
+#     doc.save(f'заявка аукцион лот№{lot_namber}.docx')
+# if proces == "Публичное предложение": 
+#     doc = DocxTemplate("Zayavka_pablic.docx")
+#     doc.render(data_from_pars)
+#     doc.save(f'заявка публичка лот№{lot_namber}.docx')
+
